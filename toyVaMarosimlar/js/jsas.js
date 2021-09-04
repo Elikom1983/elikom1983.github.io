5349a5bb537d0bdd3121da3f0214db923d5afafc
Elikom1983 <65336695+Elikom1983@users.noreply.github.com>
2021-09-04 18:00:59 +0500

    js folder add

---
+++ toyVaMarosimlar/js/jsas.js
@@ -0,0 +1,40 @@
+/*
+$(document).ready(function(e) {
+    
+
+$('.slider').slick({
+	 dots: true,
+  centerMode: true,
+  centerPadding: '60px',
+    slidesToShow:5,
+   autoplay: true,
+  autoplaySpeed: 2000,
+  variableWidth: false,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3,
+		 slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1,
+		 slidesToScroll: 1
+      }
+    }
+  ]
+});
+}); */
+
+  
+
+
